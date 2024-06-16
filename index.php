5100653771a0fe0f21cdee5ce754089678b13e96
Olegovich-Big <tsyg36665@gmail.com>
2024-06-16 21:45:18 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width = device-width, initial-scale = 1.0" />
+    <meta name="keywords" content="ключивые слова по которым браузер определяет на какую тему будет сайт" />
+    <meta name="description" content="project" />
+    <title>Project</title>
+    <link rel="stylesheet" type="text/css" href="style/HomeStyle.css" />
+</head>
+<body>
+   <div>
+        <?php include 'nav.php'; ?>
+   </div>
+</body>
+</html>
---
+++ nav.php
@@ -0,0 +1,20 @@
+<div class="header--navigation">
+    <div class="navigation__icon"> <img class="navigation__icon" src = "img/tasty_town.png" alt="IconcNavigation" /> </div>
+    <nav class="navigation__header-link">
+        <ul>
+            <li> <a class="header-link" href="https://youtu.be/p7vwdxJn8l8">О компании</a> </li>
+            <li> <a class="header-link" href="https://youtu.be/p7vwdxJn8l8">Каталог</a></li>
+            <li> <a class="header-link" href="https://youtu.be/p7vwdxJn8l8">Акции</a> </li>
+            <li> <a class="header-link" href="https://youtu.be/p7vwdxJn8l8">Вакансии</a> </li>
+            <li> <a class="header-link" href="https://youtu.be/p7vwdxJn8l8">Контакты</a> </li>
+        </ul>
+    </nav>
+    <div class="navigation__social-link">
+        <ul>
+            <li> <a class="social-link" href="https://youtu.be/p7vwdxJn8l8"><img class="social-link__img" src="" alt=""></a> </li>
+            <li> <a class="social-link" href="https://youtu.be/p7vwdxJn8l8"><img  src="" alt=""></a></li>
+            <li> <a class="social-link" href="https://youtu.be/p7vwdxJn8l8"><img src="" alt=""></a> </li>
+            <li> <a class="social-link" href="https://youtu.be/p7vwdxJn8l8"><img src="" alt=""></a> </li>
+        </ul> 
+    </div>
+</div>
