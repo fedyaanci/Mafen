ce7639d936b75135ba905fb36fa15c5ad8e2f4e5
fedyaanci <fedyafizmat@gmail.com>
2024-06-16 19:47:01 +0300

    add aboutShop1

---
+++ aboutShop.php
@@ -0,0 +1,8 @@
+<div class='aboutCompany'>
+<div class='aboutCompany_title'>
+    <div class='aboutCompany_title-name'>О компании TASTY TOWN</div>
+    <div class='aboutCompany_title-definition'>Самая крупная сеть табачных магазинов в Республике Марий-Эл</div>
+</div>
+<div class='aboutCompany_subtitle'>
+    В магазинах TASTY TOWN всегда можно оценить новинки и приятно провести время. Вы получите качественный сервис вместе с оригинальной продукцией, потому что наша команда предана своим клиентам и отвечает за каждый проданный товар.
+</div>
