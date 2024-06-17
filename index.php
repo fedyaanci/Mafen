5f2f7cc2f861476df9925b674c2ec85f33bdab79
fedyaanci <fedyafizmat@gmail.com>
2024-06-17 21:32:56 +0300

    resonShop + aboutshop without adaptive

--- index.php
+++ index.php
@@ -4,10 +4,15 @@
     <meta charset="UTF-8">
     <title>main project</title>
     <link rel="stylesheet" href="/aboutShop-Style.css">
+    <link rel="stylesheet" href="/resonShop-Style.css">
 </head>
 <body>
     <?php
         require 'aboutShop.php';
     ?>
+     <?php
+        require 'resonShop.php';
+    ?>
+    
 </body>
 </html>
---
+++ resonShop.php
@@ -0,0 +1,36 @@
+<h1 class='resonTitle'>
+    Почему стоит покупать только в TASTY TOWN?    
+</h1>
+<h2 class='resonSubtitle'>
+    Теперь мы расскажем Вам, почему нужно покупать
+    только в нашей сети магазинов.   
+</h2>
+<div class='advantage'>
+    <div class='advantage_quality'>
+        <img src='/static/img/quality.svg' width="100px">
+        <div class='advantage_quality-title'>
+            Качество
+        </div>
+        <div class='advantage_subtitle'>
+            Вы узнаете что такое по настоящему качественная продукция
+        </div>
+    </div>
+    <div class='advantage_personal'>
+        <img src='/static/img/personal.svg' width="100px">
+        <div class='advantage_personal-title'>
+            Персонал
+        </div>
+        <div class='advantage_subtitle'>
+            Наши профессионалы всегда рады будут Вам помочь и ответят на все Ваши вопросы
+        </div>
+    </div>
+    <div class='advantage_range'>
+        <img src='/static/img/range.svg' width="100px">
+        <div class='advantage_range-title'>
+            Ассортимент
+        </div>
+        <div class='advantage_subtitle'>
+            Смело заявляем что в нашей сети представлен широкий ассортимент товара
+        </div>
+    </div>
+</div>
