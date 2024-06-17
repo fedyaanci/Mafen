48e8727f20a0386065673b7c4b603f6f394ff80e
fedyaanci <fedyafizmat@gmail.com>
2024-06-17 19:04:27 +0300

    aboutshop some adaptive

--- aboutShop.php
+++ aboutShop.php
@@ -1,17 +1,20 @@
 <div class='aboutCompany'>
-        <div class='aboutCompany_title'>
-            <div class='aboutCompany_title-name'>
-                О компании <p>TASTY TOWN</p>
-            </div>
-            <div class='aboutCompany_title-description'>
-                <p>В магазинах TASTY TOWN всегда можно оценить</p>
-                <p>новинки и приятно провести время. Вы получите</p>
-                <p>качественный сервис вместе с оригинальной</p>
-                <p>продукцией, потому что наша команда предана своим</p> 
-                <p>клиентам и отвечает за каждый проданный товар.</p>
+    <div class='aboutCompany_title'>
+        <div class='aboutCompany_title-name'>
+            <div class='DesignFirstLetter'>
+                О
+            </div> 
+            компании <p>TASTY TOWN</p>
+            <div class="aboutCompany_subtitle">
+                Самая крупная сеть табачных магазинов <p>в Республике Марий-Эл</p>   
             </div>
         </div>
-        <div class="aboutCompany_subtitle">
-            Самая крупная сеть табачных магазинов <p>в Республике Марий-Эл</p>   
+        <div class='aboutCompany_title-description'>
+            <p>В магазинах TASTY TOWN всегда можно оценить</p>
+            <p>новинки и приятно провести время. Вы получите</p>
+            <p>качественный сервис вместе с оригинальной</p>
+            <p>продукцией, потому что наша команда предана своим</p> 
+            <p>клиентам и отвечает за каждый проданный товар.</p>
         </div>
     </div>
+</div>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>main project</title>
-    <link rel="stylesheet" href="/Style.css">
+    <link rel="stylesheet" href="/aboutShop-Style.css">
 </head>
 <body>
     <?php
